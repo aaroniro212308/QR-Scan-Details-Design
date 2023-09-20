f494fb2d635f6595c2f69ada4ee52161043ba2dc
User <User@DESKTOP-PPDFD4L>
2023-09-20 09:15:10 +0530

    sa

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
                       required
                     />
                   </div>
-                  <div class="col-6">
+                  <div class="col-12">
                     <div class="row">
                       <div class="col-md-12 text-center">
                         <button
